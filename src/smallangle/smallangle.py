793ed94050132e12f69208f899ae5dbc3c259059
BrandonFrankevoort <135611475+Brandongithubding@users.noreply.github.com>
2023-11-16 11:49:54 +0100

    Added docstrings for the functions

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -17,6 +17,14 @@ def cmd_group():
 )
 # Gives values of sin for "number" values equally seperated in [0, 2pi].
 def sin(number):
+    """Gives values of sin for "number" values equally seperated in [0, 2pi].
+
+    Args:
+        number (int): the number of values used from the interval.
+
+    Side Effects:
+        Prints the sin(x) values next to the x values from the interval.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -29,8 +37,16 @@ def sin(number):
     "--number",
     default=10,
 )
-# Gives values of tan for "number" values equally seperated in [0, 2pi].
+# 
 def tan(number):
+    """Gives values of tan for "number" values equally seperated in [0, 2pi].
+
+    Args:
+        number (int): the number of values used from the interval.
+
+    Side Effects:
+        Prints the tan(x) values next to the x values from the interval.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
@@ -48,6 +64,17 @@ def tan(number):
 # This is the highest angle for which x can be used instead of sin(x)
 # while still keeping the wanted amount of accuracy.
 def approx(accuracy, decimals):
+    """Gives the highest angle so that the small-angle inequality holds.
+
+    Args:
+        accuracy (float): The accuracy used in the small-angle inequality.
+        
+    Options:
+    -n decimals (int): The precision in decimals that is used to approximate.
+
+    Side Effects:
+        Prints the small-angle approximation.
+    """
     x = 0
     accuracy = float(accuracy)
     # Keep increasing until the inequality no longer holds.
