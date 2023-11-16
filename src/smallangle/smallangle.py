87a391adf4b50b5e23fc29f9423ff2cf36fe7cc0
BrandonFrankevoort <135611475+Brandongithubding@users.noreply.github.com>
2023-11-16 09:57:50 +0100

    Made the subcommands

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,20 +3,34 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+@cmd_group.command()
+@click.option(
+    "-a",
+    "--amount",
+    default=10,
+)
+def sin(amount):
+    x = np.linspace(0, 2 * pi, amount)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+@cmd_group.command()
+@click.option(
+    "-a",
+    "--amount",
+    default=10,
+)
+def tan(amount):
+    x = np.linspace(0, 2 * pi, amount)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
